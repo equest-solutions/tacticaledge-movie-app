d35a39c141e8f5229ffe4db856b1810a0d491faa
harshil-eQuest <harshil.sonpal@equestsolutions.net>
2023-12-28 17:44:03 +0530

    changes modified and create movieList api

--- backend/src/middlewares/authentication.js
+++ backend/src/middlewares/authentication.js
@@ -17,23 +17,22 @@ const generateToken = (userJson) => {
   console.log('userJson', userJson);
   const payload = {
     id: userJson.id,
-    email: userJson.email
+    email: userJson.email,
+    user_uuid: userJson.uuid
   };
-  // Set the expiration time to one day (1 day)
+  // Set the expiration time to one hour (1 day)
   const expiresIn = '1d';
 
   // Create the JWT token with the user ID and expiration time
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
-  console.log('token++++', token);
 
   return token;
 };
 
 const isTokenValid = (token) => {
   try {
-    console.log('Is Valid token', token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('decoded', decoded);
+    console.log("decoded Token" , decoded)
     return { success: true, data: decoded };
   } catch (error) {
     return { success: false, error: error.message };
@@ -51,27 +50,24 @@ const authenticateToken = async (req, res, next) => {
 
   try {
     const user = await userModel.getUserByIdOrToken(null, token);
-    console.log('user:-', user);
-    console.log('userToken:-', user.token);
-
     // Verify the token using the secret key
     // const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Check if the token has not expired
     // if (decoded.exp && decoded.exp > Math.floor(Date.now() / 1000)) {
     // Check if the token stored in the database matches the provided token
-
     if (user && user.token === token) {
+      req.body.auth_user =  user ; 
       return next();
     } else {
-      return res.status(401).send('Token has expired');
+      return res.status(401).send({ status : false , message: 'Token has been expired' });
     }
     // } else {
     //   return res.status(401).send('Token has expired');
     // }
   } catch (error) {
     // Token verification failed
-    return res.status(401).send('Invalid Token');
+    return res.status(401).send({ status : false ,  message : error.message });
   }
 };
 
