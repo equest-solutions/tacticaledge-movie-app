b773e896970d7501876827db92f068b35e02a27e
harshil-eQuest <harshil.sonpal@equestsolutions.net>
2024-01-04 15:06:42 +0530

    Update database.js
    database connections changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,20 +3,39 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const db = mysql.createConnection({
+const dbConfig = {
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-})
+};
+
+let db;
 
 // connect to the MySQL database
-db.connect((error) => {
-  if (error) {
-    console.error('Error connecting to MySQL database:', error)
-  } else {
-    console.log('Connected to MySQL database!')
-  }
-})
+function handleDisconnect () {
+  db = mysql.createConnection(dbConfig);
+
+  db.connect(function (err) {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('Database is connected!');
+    }
+  });
+
+  db.on('error', function (err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect()
 
 module.exports = db
